178d81045effd178ae5fd6f17ea2a6019539c7d2
Avinash <avinashmarbhal1994@gmail.com>
2026-03-02 12:28:55 +0530

    new navbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { DisableRightClickDirective } from './directives/disable-right-click.directive';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, DisableRightClickDirective],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, DisableRightClickDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,55 @@
+import { NgClass } from '@angular/common';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Constants } from '../../models/constants';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [NgClass, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent implements OnInit {
+  @ViewChild('navbarCollapse') navbarCollapse!: ElementRef;
+
+  isDarkMode: boolean = false;
+  isMenuOpen: boolean = false;
+
+  appName1: string = Constants.APP_NAME_STR1;
+  appName2: string = Constants.APP_NAME_STR2;
+
+  navLinks = Constants.NAV_LINKS;
+
+  ngOnInit() {
+    // Load the manually saved theme or default to light
+    const savedTheme = localStorage.getItem('user-theme') || 'light';
+    this.isDarkMode = savedTheme === 'dark';
+    this.applyTheme();
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    const theme = this.isDarkMode ? 'dark' : 'light';
+    localStorage.setItem('user-theme', theme);
+    this.applyTheme();
+    this.closeMenu();
+  }
+
+  private applyTheme() {
+    const theme = this.isDarkMode ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-bs-theme', theme);
+  }
+
+  // toggleMenu() {
+  //   this.isMenuOpen = !this.isMenuOpen;
+  // }
+
+  closeMenu() {
+    if (this.navbarCollapse.nativeElement.classList.contains('show')) {
+      this.navbarCollapse.nativeElement.classList.remove('show');
+      this.isMenuOpen = !this.isMenuOpen;
+    } else {
+      this.isMenuOpen = !this.isMenuOpen;
+    }
+  }
+}
